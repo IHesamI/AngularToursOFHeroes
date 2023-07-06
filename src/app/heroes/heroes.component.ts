db4c1ef8a08894386bab11ade326033e8ea47aa7
hesam <bhesam2000@yahoo.com>
2023-07-06 10:32:27 +0430

    try to add a service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero:Hero={
-    id:1,
-    name:'Hesam',
+  heroes = HEROES;
+  selectedHero?:Hero;
+  onSelector(hero:Hero):void{
+    this.selectedHero=hero;
   }
 }
